5579ce14b22d7e66220f8336963f1679e66d8657
Herman Henriksen <hermanimittveien@gmail.com>
2023-03-20 13:17:05 +0100

    first commit

---
+++ tdt4240-diving-duck-server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace tdt4240_diving_duck_server;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
+
