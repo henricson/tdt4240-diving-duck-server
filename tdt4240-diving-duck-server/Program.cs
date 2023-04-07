c40cf1b3597ec3311a687fbc8745b77287b81bd9
Herman Henriksen <hermanimittveien@gmail.com>
2023-04-08 01:16:10 +0200

    Fixes

--- tdt4240-diving-duck-server/Program.cs
+++ tdt4240-diving-duck-server/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddApplicationInsightsTelemetry();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<MyContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
